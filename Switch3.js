b7c55cbdfd0c424f0af21317b236c485973d9792
francoisvaillant <frvaillant@gmail.com>
2020-06-03 01:37:50 +0200

    add optionnal name

--- Switch3.js
+++ Switch3.js
@@ -1,6 +1,6 @@
 class Switch3 {
 
-    constructor(labels, values, renderZoneId, initVal= 0, destinationInputId = null) {
+    constructor(labels, values, renderZoneId, initVal= 0, destinationInputId = null, name = 'switch3') {
         this.labels         = labels;
         this.values         = values;
         this.renderZone     = document.getElementById(renderZoneId);
@@ -13,14 +13,15 @@ class Switch3 {
         this.leftPosition   = 0;
         this.centerPosition = 1;
         this.rightPosition  = 2;
+        this.name           = name;
     }
 
     init() {
         this.render();
-        this.bar            = document.getElementById('switch3-bar');
-        this.cursor         = document.getElementById('switch3-cursor');
-        this.field          = (this.inputId) ? document.getElementById(this.inputId) : document.getElementById('switch3-val');
-        this.labels         = document.getElementsByClassName('switch3-label');
+        this.bar            = document.getElementById(this.name + '-bar');
+        this.cursor         = document.getElementById(this.name + '-cursor');
+        this.field          = (this.inputId) ? document.getElementById(this.inputId) : document.getElementById(this.name + '-switch3-val');
+        this.labels         = document.getElementsByClassName(this.name + '-label');
         const barDimensions = this.bar.getBoundingClientRect();
         this.left           = barDimensions.left;
         this.right          = barDimensions.right;
@@ -52,15 +53,15 @@ class Switch3 {
     }
 
     createElement() {
-        let html = '<div class="switch3-container" id="switch3-container">\n' +
-                    '    <div class="switch3-bar" id="switch3-bar">\n' +
-                    '      <div class="switch3-cursor pos-' + this.initVal + ' " id="switch3-cursor"></div>\n' +
-                    '    </div>\n' +
-                    '    <div class="switch3-label" data-value="'+ this.values[this.leftPosition] +'" id="label0">' + this.labels[this.leftPosition] + '</div>\n' +
-                    '    <div class="switch3-label" data-value="'+ this.values[this.centerPosition] +'" id="label1">' + this.labels[this.centerPosition] + '</div>\n' +
-                    '    <div class="switch3-label" data-value="'+ this.values[this.rightPosition] +'" id="label2">' + this.labels[this.rightPosition] + '</div>\n' ;
+        let html = '<div class="switch3-container" id="' + this.name + '-container">\n' +
+            '    <div class="switch3-bar" id="' + this.name + '-bar">\n' +
+            '      <div class="switch3-cursor pos-' + this.initVal + ' " id="' + this.name + '-cursor"></div>\n' +
+            '    </div>\n' +
+            '    <div class="switch3-label" data-value="'+ this.values[this.leftPosition] +'" id="' + this.name + '-label0">' + this.labels[this.leftPosition] + '</div>\n' +
+            '    <div class="switch3-label" data-value="'+ this.values[this.centerPosition] +'" id="' + this.name + '-label1">' + this.labels[this.centerPosition] + '</div>\n' +
+            '    <div class="switch3-label" data-value="'+ this.values[this.rightPosition] +'" id="' + this.name + '-label2">' + this.labels[this.rightPosition] + '</div>\n' ;
         if (!this.inputId) {
-            html += '<input type="hidden" id="switch3-val" name="switch3-val">';
+            html += '<input type="hidden" id="' + this.name + '-switch3-val" name="' + this.name + '-switch3-val">';
         }
         html += '    </div>';
         return html;
@@ -75,7 +76,7 @@ class Switch3 {
 
     addActiveLabels(position) {
         for(let i=0; i<this.labels.length; i++) {
-            if (this.labels[i].id === 'label'+position) {
+            if (this.labels[i].id === this.name + '-label'+position) {
                 this.labels[i].classList.add('active')
             }
         };
